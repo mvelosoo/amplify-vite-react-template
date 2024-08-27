1f390da0a05473ff256cfb254cb1ae1bdc588ad3
Maria-Eduarda-Sauter <152434312+Maria-Eduarda-Sauter@users.noreply.github.com>
2024-08-27 12:17:02 -0300

    Update 1 App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,68 +10,50 @@ function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+
+    return () => subscription.unsubscribe();
   }, []);
 
-   <Authenticator>
+  function createTodo() {
+    client.models.Todo.create({ content: "New Todo" });
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
+  return (
+    <Authenticator>
       {({ signOut }) => (
         <main>
-          {/*...*/}
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map(todo => (
+              <li
+                onClick={() => deleteTodo(todo.id)}
+                key={todo.id}
+              >
+                {todo.content}
+              </li>
+            ))}
+          </ul>
           <button onClick={signOut}>Sign out</button>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
         </main>
       )}
     </Authenticator>
-  )
-}
-
-   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map(todo => <li
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
-      </div>
-    </main>
-  )
-}
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
   );
 }
 
 export default App;
+
