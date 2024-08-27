d4daf7613f70154535cc54a2d6ab3ed2bae6b55c
Maria-Eduarda-Sauter <152434312+Maria-Eduarda-Sauter@users.noreply.github.com>
2024-08-27 12:04:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,30 @@ function App() {
     });
   }, []);
 
+   function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+  return (
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map(todo => <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
+      </div>
+    </main>
+  )
+}
+
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
